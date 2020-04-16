def8051cf8fb46589349698d2491c7b40137ae3c
Muhammad Jihad Robbani <jihad.robbani@yahoo.com>
2020-04-16 10:18:04 +0700

    Create Exercise7-HS.js

---
+++ Exercise7-HS.js
@@ -0,0 +1,17 @@
+// Exercise 7 Hapus Simbol
+function hapusSimbol(str) {
+    var database = /[a-z0-9]+/gi;
+    var arr = str.match(database);
+    var newStr = '';
+    for (let i = 0; i < arr.length; i++) {
+        newStr += arr[i];
+    }
+    return newStr;
+  }
+  
+  // TEST CASES
+  console.log(hapusSimbol('test%$4aa')); // test4aa
+  console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
+  console.log(hapusSimbol('ma@#k!an~')); // makan
+  console.log(hapusSimbol('coding')); // coding
+  console.log(hapusSimbol('1+3-5*2=100')); // 1352100
